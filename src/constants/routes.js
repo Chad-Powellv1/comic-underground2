3d76dd406d6b2e74e0d14f7fb3062c14ba11e1d2
Chad Powell <chad.powell@student.awesomeinc.org>
2022-05-03 00:31:25 +0000

    created routes and storage constants

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const HOME = '/';
+export const SELL = '/sell';
+export const SEARCH = '/search';
+export const WATCHLIST = '/watchlist';
+export const CART = '/cart';
+export const SELLER = '/seller';
+export const DETAIL = '/detail';
