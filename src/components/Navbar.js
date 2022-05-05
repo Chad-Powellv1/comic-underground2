e6f3f1875ecb37a5ee3b198a8cc06708454cc592
Chad Powell <chad.powell@student.awesomeinc.org>
2022-05-05 20:46:35 +0000

    completed logout feature with working state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiFillWindows, AiOutlineSearch } from 'react-icons/ai';
 import comic from '../assets/comic.png';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import React from 'react';
+import {useGlobalState} from '../context/GlobalState';
+import {AuthService} from '../services/auth.service';
 
-export const Navbar = ({state}) => {
+const logout = () => {
+    localStorage.clear()
+    window.location.reload();
+}
+
+export const Navbar = () => {
+    const [ state ] = useGlobalState();
   return (
     <>
+        { state && (
         <div className='d-flex flex-row ml-3 p-3'>
             <div className='mr-auto'>
                 <div className='header-wrapper'>
@@ -14,20 +24,36 @@ export const Navbar = ({state}) => {
             </div>
         
         <div className='header-link'>
-            <Link to='/login'style={{textDecoration: 'none', fontFamily: 'var(--ff-arial)', }}><span className="header-sell">Login</span></Link>
+            { !state.currentUser && (
+            <Link to='/login'style={{textDecoration: 'none', fontFamily: 'var(--ff-arial)', }}><span className="header-sell">Login</span></Link>)}
+            { state.currentUser && (
+            <button onClick={() => {logout()}}
+                style={{
+                    backgroundColor: 'transparent',
+                    fontFamily: 'var(--ff-arial)',
+                    border: 0,
+                    marginBottom: '1.5rem',
+                }}
+            >
+            <span className="header-sell">Logout</span>
+            </button>)}
             <Link to='/register' style={{textDecoration: 'none', fontFamily: 'var(--ff-arial)'}}><span className="header-sell">Register</span></Link>
             {/* <Link to='/sell' style={{textDecoration: 'none', fontFamily: 'var(--ff-arial)'}}><span className="header-sell">Sell</span></Link>
             <Link to='/watchlist' style={{textDecoration: 'none', fontFamily: 'var(--ff-arial)'}}><span className="header-sell">Watchlist</span></Link> */}
             <Link to="/about" style={{textDecoration: 'none', color:'black'}}>About Us</Link>
         </div>
-        </div>
+        </div>)}
+
+        { state && (
         <div className='header'>
         <img className='header-logo' src={comic} alt='Comic Underground icon' />
             <div className='header-search'>
             <input className='header-searchInput' type='text' placeholder="Search"/>
             <AiOutlineSearch className='header-searchIcon' />
             </div>
-        </div>
+        </div>)}
+
+
     </>
   )
 }
--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -7,7 +7,7 @@ export const Banner = () => {
         <img src={image} alt='banner'
             style={{
                 width:'100%',
-                height:'15rem',
+                height:'12.5rem',
             }}  />
         <div className='centered text-centered'>
             <h1 className='text'>Comic Underground</h1>
